e414a53e3f227c44e1bf5c6af0ac3aafc2aa8d70
abyeol <choiabyeol@gmail.com>
2024-01-09 13:15:29 +0900

    aaaa

--- skeleton_front/src/App.jsx
+++ skeleton_front/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Header from './home/component/Header'
+import Footer from './home/component/Footer'
+import HomeMain from './home/HomeMain'
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {/* Header, Footer 가 전체 화면에서 항상 고종으로 나온다면.. 아래처럼
+      각 화면별로 header, Footer 가 상이하게 적용된다면 Router 되는 Com
+       */}
+      <Header/>
+      {/*
+      
+      */}
+      <Routes>
+        <Route path='/' element={<HomeMain/>}/>
+      </Routes>
+      <Footer/>
+    </div>
   )
 }
 
